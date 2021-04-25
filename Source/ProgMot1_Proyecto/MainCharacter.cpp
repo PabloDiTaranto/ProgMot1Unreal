bc420a1130451f725894aa7b09286be326b24a67
Pablo Di Taranto <73130438+PabloDiTaranto@users.noreply.github.com>
2021-04-25 16:35:27 -0300

    BaseCharacterMovementC++

---
+++ Source/ProgMot1_Proyecto/MainCharacter.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainCharacter.h"
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("MouseX", this, &AMainCharacter::MouseX);
+	PlayerInputComponent->BindAxis("MouseY", this, &AMainCharacter::MouseY);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMainCharacter::Jump);
+}
+
+void AMainCharacter::MoveForward(float f)
+{
+	if (Controller && f != 0) 
+	{
+		FRotator rot = GetController()->GetControlRotation();
+		FRotator myYaw(0, rot.Yaw, 0);
+		FVector dir = FRotationMatrix(myYaw).GetUnitAxis(EAxis::X);
+		AddMovementInput(dir, f);
+	}
+}
+
+void AMainCharacter::MoveRight(float f)
+{
+	if (Controller && f != 0) 
+	{
+		FRotator rot = GetController()->GetControlRotation();
+		FRotator myYaw(0, rot.Yaw, 0);
+		FVector dir = FRotationMatrix(myYaw).GetUnitAxis(EAxis::Y);
+		AddMovementInput(dir, f);	
+	}
+}
+
+void AMainCharacter::MouseX(float f)
+{
+	AddControllerYawInput(f);
+}
+
+void AMainCharacter::MouseY(float f)
+{
+	AddControllerPitchInput(f);
+}
+
